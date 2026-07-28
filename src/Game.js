62e756f70ec6aed66c070bfb2bba6f2aa8b0ce65
Toby <FreTo2022@gmail.com>
2026-07-28 18:42:13 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,45 @@ import { TurnOrder } from "boardgame.io/core"
 /** @type {Game} */
 export const Game = {
   setup: ({ random, ctx }) => {
-    return { test: "abc", cells: ["test"] }
+    let score = [0, 0, 0, 0]
+    let geldstapel = []
+    // Geldstapel wird befüllt
+      let geldzahlen = [1,2,3,4,5,6,7,8,9]
+      let geldfarben = ["blau", "gelb", "grün", "orange"]
+      for (let x = 0; x < 3; x++) {
+        for (const zahl of geldzahlen) {
+          for (const farbe of geldfarben) {
+           let neueKarte = {farbe: farbe, zahl: zahl}
+           geldstapel.push(neueKarte)
+      }
+      }
+      }
+      // Geldstapel wird befüllt, fertig
+
+      let hausstapel = []
+      // hausstapel füllen
+      let hausfarben = ["blau", "rot", "braun", "weiß", "gruen", "lila"]
+
+       for (let i = 0; i < 7; i++) {
+       let x = 8
+       for (const hausfarbe of hausfarben) {
+       let neueHauskarte = {farbe: hausfarbe, zahl: x }
+       x= x+1
+       hausstapel.push(neueHauskarte)
+
+
+      }
+      }
+      for (let index = 0; index < 2; index++) {
+        hausstapel.push({farbe: "braun", zahl: 10 },{farbe: "weiß", zahl: 11 })
+
+      }
+     for (let index = 0; index < 4; index++) {
+        hausstapel.push({farbe: "gruen", zahl: 12 },{farbe: "lila", zahl: 13 })
+
+      }
+      //hausstapel füllen, fertig
+    return { score: score, geldstapel: geldstapel, hausstapel: hausstapel }
   },
 
   moves: {
@@ -31,5 +69,5 @@ export const Game = {
   disableUndo: true,
 
   endIf: ({ G, ctx, random }) => {},
-
 }
+
