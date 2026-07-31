7f3772447f6c8d76cb2e9bd86a55d85396db4fbc
Toby <FreTo2022@gmail.com>
2026-07-31 18:31:19 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -151,14 +151,6 @@ export const Game = {
       G.spielerhände[playerID].push(G.geldmarkt[z].pop())
       if (G.geldmarkt[z][0].zahl < 5) {
         let x = G.geldmarkt[z][0].zahl
-        clickGeld: ({ G, ctx, playerID, events, random }, z) => {
-          if ((G.geldmarkt[z][0].zahl + x) < 5) {
-            G.spielerhände[playerID].push(G.geldmarkt[z].pop()
-            events.endTurn()
-          } else {
-            events.endTurn()
-          }
-        }
       } else {
         events.endTurn()
       }
