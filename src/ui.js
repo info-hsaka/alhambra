1dace94b01749edae392e8096bcf1c2f3df6de5f
Toby <FreTo2022@gmail.com>
2026-08-04 10:30:51 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -206,23 +206,31 @@ export function draw(
   //Alhambragitter(links) 2 spieler
   for (let spalte = 0; spalte < 5; spalte = spalte + 1) {
     for (let zeile = 0; zeile < 5; zeile++) {
-      //ctx.strokeStyle = "black"
+      ctx.strokeStyle = "black"
       //ctx.strokeRect(spalte * 150 + 350, zeile * 150 + 1200, 150, 150)
       if (state.G.spielfeld[zeile][spalte].length != 0) {
         if (state.G.hausangeklickt == true && state.ctx.currentPlayer == 0) {
           let spalteNeu = spalte - 1
+          ctx.strokeRect(spalteNeu * 150 + 350, zeile * 150 + 1200, 150, 150)
+
           onClick(spalteNeu * 150 + 350, zeile * 150 + 1200, 150, 150, () => {
             moves.clickFeld(spalteNeu, zeile)
           })
           let spalteNeu1 = spalte + 1
+          ctx.strokeRect(spalteNeu1 * 150 + 350, zeile * 150 + 1200, 150, 150)
+
           onClick(spalteNeu1 * 150 + 350, zeile * 150 + 1200, 150, 150, () => {
             moves.clickFeld(spalteNeu1, zeile)
           })
           let zeileNeu = zeile - 1
+          ctx.strokeRect(spalte * 150 + 350, zeileNeu * 150 + 1200, 150, 150)
+
           onClick(spalte * 150 + 350, zeileNeu * 150 + 1200, 150, 150, () => {
             moves.clickFeld(spalte, zeileNeu)
           })
           let zeileNeu1 = zeile + 1
+          ctx.strokeRect(spalte * 150 + 350, zeileNeu1 * 150 + 1200, 150, 150)
+
           onClick(spalte * 150 + 350, zeileNeu1 * 150 + 1200, 150, 150, () => {
             moves.clickFeld(spalte, zeileNeu1)
           })
