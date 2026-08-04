752126a4e880e2b35da905791d7b0242b455c3c9
Toby <FreTo2022@gmail.com>
2026-08-04 09:49:28 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -208,6 +208,8 @@ export function draw(
     for (let zeile = 0; zeile < 5; zeile++) {
       ctx.strokeStyle = "black"
       ctx.strokeRect(spalte * 150 + 350, zeile * 150 + 1200, 150, 150)
+      if (state.G.spielfeld[spalte][zeile] != 0) {
+      }
       if (state.G.hausangeklickt == true && state.ctx.currentPlayer == 0) {
         onClick(spalte * 150 + 350, zeile * 150 + 1200, 150, 150, () => {
           moves.clickFeld(spalte, zeile)
