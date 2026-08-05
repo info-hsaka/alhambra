ac1fca1bc84804dceafa2070e7337af68b50cda8
Toby <FreTo2022@gmail.com>
2026-08-05 11:10:12 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -456,6 +456,7 @@ export function draw(
   for (let spalte = 0; spalte < 5; spalte++) {
     for (let zeile = 0; zeile < 5; zeile++) {
       if (state.G.spielfeld[zeile][spalte].length != 0) {
+        console.log(state.G.spielfeld[zeile][spalte])
         gitter(
           state.G.spielfeld[zeile][spalte][0].farbe,
           state.G.spielfeld[zeile][spalte][0].zahl,
